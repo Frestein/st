f4e4ea4e0a50d0afd997fc8aa0edba6ad0cbc745
Frestein <Frestein@tuta.io>
2024-06-20 03:18:21 +0500

    feat(newterm): add st-newterm-0.9-tabbed patch

--- patch/newterm.c
+++ patch/newterm.c
@@ -1,4 +1,5 @@
 void newterm(const Arg *a) {
+	char *tabbed_win;
   switch (fork()) {
   case -1:
     die("fork failed: %s\n", strerror(errno));
@@ -11,7 +12,11 @@ void newterm(const Arg *a) {
       break;
     case 0:
       chdir_by_pid(pid);
-      execl("/proc/self/exe", argv0, NULL);
+			tabbed_win = getenv("XEMBED");
+			if (tabbed_win)
+				execl("/proc/self/exe", argv0, "-w", tabbed_win, NULL);
+			else
+				execl("/proc/self/exe", argv0, NULL);
       _exit(1);
       break;
     default:
